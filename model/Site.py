ee965bdb37f55eff588ee65cf67d9d63775af160
Even <yw4930@nyu.edu>
2022-11-30 13:51:29 -0500

    Update Site.py

--- model/Site.py
+++ model/Site.py
@@ -1,6 +1,6 @@
+from copy import deepcopy
 from model.DataManager import DataManager
 from model.LockManager import LockManager
-from copy import deepcopy
 
 
 class Site(object):
@@ -18,17 +18,23 @@ class Site(object):
         self.data_manager.disable_accessibility()
 
     def echo(self):
-        prefix = f"Site {self.site_id} ({'up' if self.up else 'down'})"
-        return [prefix] + [v for v in self.data_manager.data]
+        if self.up == True:
+            prefix = f"Site {self.site_id} ({'up'})"
+        else:
+            prefix = f"Site {self.site_id} ({'down'})"
+        a = [prefix]
+        for v in self.data_manager.data:
+            a = a + [v]
+        return a
 
     def recover(self):
         self.up = True
 
     def snapshot(self, tick):
         available_data = {}
-        for idx, d in enumerate(self.data_manager.data):
-            if d and self.data_manager.is_accessible[idx]:
-                available_data[idx + 1] = d
+        for idx, d in enumerate(self.data_manager.data, start=1):
+            if self.data_manager.is_accessible[idx] == True and d:
+                available_data[idx] = d
         self.snapshots[tick] = deepcopy(available_data)
 
     def get_snapshot_variable(self, tick, var_id):
