0583d81258bba37bd2b95fe691af4e4ba1f78cf6
Even <yw4930@nyu.edu>
2022-11-30 17:30:09 -0500

    Update DeadLockDetector.py

--- algo/DeadLockDetector.py
+++ algo/DeadLockDetector.py
@@ -7,60 +7,58 @@ class WaitFor(object):
 
     def add_operation(self, operation):
         op_t = operation.get_op_t()
-        para = operation.get_parameters()
 
-        if not (op_t == "R" or op_t == "W"):
+        if op_t != "R" and op_t != "W":
             return
 
-        trans_id = para[0]
-        var_id = para[1]
+        trans_id = operation.get_parameters()[0]
+        var_id = operation.get_parameters()[1]
 
-        if self.tm.transactions[trans_id].is_readonly:
+        if self.tm.transactions[trans_id].is_readonly == True:
             return
         ops = self.var_to_ops.get(var_id, set())
 
-        # Case 1: operation is W
         if op_t == "W":
             for op in ops:
-                if op.get_parameters()[0] == trans_id and op.get_op_t() == "W":
+                para1 = op.get_parameters()[0]
+                if op.get_op_t() == "W" and para1 == trans_id:
                     ops.add(operation)
                     self.var_to_ops[var_id] = ops
                     return
-
             for op in ops:
-                if op.get_parameters()[0] != trans_id:
+                para1 = op.get_parameters()[0]
+                if para1 != trans_id:
                     waits = self.wait_for.get(trans_id, set())
-                    waits.add(op.get_parameters()[0])
+                    waits.add(para1)
                     self.wait_for[trans_id] = waits
-        # Case 2: operation is W
-        else:
+        elif op_t == "R":
             for op in ops:
-                if op.get_parameters()[0] == trans_id:
+                para1 = op.get_parameters()[0]
+                if para1 == trans_id:
                     ops.add(operation)
                     self.var_to_ops[var_id] = ops
                     return
-
             for op in ops:
-                if op.get_op_t() == "W" and op.get_parameters()[0] != trans_id:
+                para1 = op.get_parameters()[0]
+                if para1 != trans_id and op.get_op_t() == "W":
                     waits = self.wait_for.get(trans_id, set())
-                    waits.add(op.get_parameters()[0])
+                    waits.add(para1)
                     self.wait_for[trans_id] = waits
         ops.add(operation)
         self.var_to_ops[var_id] = ops
 
     def _recursive_check(self, cur_node, target, visited, trace):
         visited[cur_node] = True
-
         if cur_node not in self.wait_for:
             return False
         trace.append(cur_node)
         neighbor_nodes = self.wait_for[cur_node]
         for neighbor in neighbor_nodes:
-            if neighbor == target:
-                return True
-            elif neighbor not in visited:
+            if neighbor not in visited:
                 continue
-            elif not visited[neighbor]:
+            elif neighbor == target:
+                return True
+            elif visited[neighbor] == False:
                 if self._recursive_check(neighbor, target, visited, trace):
                     return True
         trace.pop(-1)
@@ -71,7 +69,9 @@ class WaitFor(object):
         self.trace = []
 
         for target in nodes:
-            visited = {node: False for node in nodes}
+            visited = {}
+            for node in nodes:
+                visited[node] = False
             if self._recursive_check(target, target, visited, self.trace):
                 return True
         return False
@@ -81,7 +81,10 @@ class WaitFor(object):
 
     def remove_transaction(self, transaction_id):
         for var, ops in self.var_to_ops.items():
-            ops = {op for op in ops if op.get_parameters()[0] != transaction_id}
+            ops = {}
+            for op in ops:
+                if op.get_parameters()[0] != transaction_id:
+                    ops.add(op)
             self.var_to_ops[var] = ops
 
         self.wait_for.pop(transaction_id, None)
