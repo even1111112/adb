5c6523a6c0256fab2101d6b0ca1f047d3f0c4034
Even <yw4930@nyu.edu>
2022-12-01 08:49:22 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,61 @@
-# This is a sample Python script.
-
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')
+from utils.FileLoader import FileLoader
+from utils.driver import run, run_interactive
+import argparse
+import sys
+import os
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def run_file(input_file, output_file):
+    with open(output_file, "w") as file:
+        sys.stdout = file
+        loader = FileLoader(input_file)
+        case_id = 1
+        while loader.has_next() is not None:
+            print("Test" + str(case_id) + "Result")
+            run(loader.next_case())
+            case_id = case_id + 1
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+def runi():
+    run_interactive()
+
+
+def runf(input, output):
+    run_file(input, output)
+
+
+def rund(input, output):
+    files = os.listdir(input)
+
+    try:
+        os.mkdir(output)
+    except Exception as e:
+        print("Directory has existed, ignore")
+
+    for file in files:
+        if file.endswith(".txt") == True:
+            input_file = os.path.join(input, file)
+            output_file = os.path.join(output, file)
+            run_file(input_file, output_file)
+
+
+rundict = {
+    "i": runi,
+    "f": runf,
+    "d": rund
+}
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser("RepCRec")
+    parser.add_argument("mode", type=str, help="program mode (f/d/i), 'i' represents interactive mode")
+    parser.add_argument("-input", type=str, help="input source")
+    parser.add_argument("-output", type=str, help="output source")
+    args = parser.parse_args()
+
+    mode = args.mode
+    input_src = args.input
+    output_src = args.output
+
+    fun = rundict.get(mode)
+    fun()
+
