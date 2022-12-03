54c245d463afd7efc7223b145f4515ca4ee60c0f
Yusu Qian <yq729@nyu.edu>
2022-12-02 21:44:35 -0500

    Update driver.py

--- utils/driver.py
+++ utils/driver.py
@@ -1,34 +1,24 @@
-from configurations import *
 from model.Site import Site
-from model.TransactionManager import TransactionManager
+from model.managers.TransactionManager import TransactionManager
 from model.Operation import OperationParser, OperationCreator
-
-
-def init_sites():
-
-    sites = []
-    for i in range(1, number_of_sites + 1):
-      sites.append(Site(i))
-    return sites
-
 
 def run(case):
     tm = TransactionManager()
-    tm.attach_sites(init_sites())
-
-
-    for i in range(len(case)):
-        op, para = OperationParser.parse(case[i])
-        operation = OperationCreator.create(op, para)
+    sites = []
+    for i in range(1, 10 + 1):
+      sites.append(Site(i))
+    tm.attach_sites(sites)
+    length = len(case)
+    tick = length + 1
+    for i in range(length):
+        operation = OperationCreator.create(OperationParser.parse(case[i])[0], OperationParser.parse(case[i])[1])
         tm.step(operation, i + 1)
-
-    tick = len(case) + 1
-    while tm.blocked == True:
-        prev_size = len(tm.blocked)
+    block_size = len(tm.blocked)
+    while tm.blocked:       
         tick += 1
         tm.retry(tick)
-        new_size = len(tm.blocked)
-
-        if prev_size == new_size:
+        if len(tm.blocked) == block_size:
             print("error: not terminable")
             break
+        else:
+            block_size = len(tm.blocked)
