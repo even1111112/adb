5b16e9df11c468c52beda6ba19ba3090904ff208
Yusu Qian <yq729@nyu.edu>
2022-12-02 21:10:28 -0500

    Update FileLoader.py

--- utils/FileLoader.py
+++ utils/FileLoader.py
@@ -5,9 +5,8 @@ class FileLoader(object):
         self._buffer_idx = -1
 
         line_count = 0
-        with open(file_name, 'r') as f:
-          
-            for line in f.readlines():
+        with open(file_name, 'r') as filename:         
+            for line in filename.readlines():
                 if line[:2] != "//":  
                     self._lines.append(line.strip())
                     line_count += 1
@@ -17,17 +16,14 @@ class FileLoader(object):
 
     def next_case(self):
 
-        operations = []
+        res = []
         for line in self._lines[self._buffer_idx + 1:]:
             self._buffer_idx += 1
-
-            if line.strip() == "<END>":
-                break
-            elif line.strip() != '':
-                if line[:2] != '//':
-                   operations.append(line.strip())
-
-        return operations
+            stripped = line.strip()
+            if stripped != ('' or "<END>"):
+              if line[:2] != '//':
+                res.append(stripped)
+        return res
 
     def has_next(self):
         if self._buffer_idx < self._end_idx:
