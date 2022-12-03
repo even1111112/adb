ffd90ee34af028176fa57db1c9ed9c627dbdf6d2
Yusu Qian <yq729@nyu.edu>
2022-12-02 20:14:58 -0500

    Update __init__.py

--- model/__init__.py
+++ model/__init__.py
@@ -1,246 +1,44 @@
-import re
-from . import Operation, parse_variable_id, do_read
-from model.Transaction import Transaction
 from tabulate import tabulate
 
-TABLE_HEADERS = ["Site Name"] 
-for i in range(20):
-  TABLE_HEADERS.append("x"+str(i+1))
+class Operation(object):
+    def __init__(self, para: [str]):
+        self.para = para
+        self.op_t = None
 
-class OperationParser(object):
-    @staticmethod
-    def parse(line):
-        res = re.search("(.*)\((.*?)\)", line)
-        p1 = res.group(1)
-        p2 = []
-        for i in range(len(res.group(2).split(","))):
-          p2.append(res.group(2).split(",")[i].strip())
-        return p1, p2
-
-class Begin(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "begin")
-
-    def execute(self, tick, tm, retry=False):
-
-        if Transaction(self.para[0], tick).transaction_id not in tm.transactions:
-          tm.transactions[Transaction(self.para[0], tick).transaction_id] = Transaction(self.para[0], tick)
-          return True
-        raise KeyError("Dupilcated transaction" + str(Transaction(self.para[0], tick).transaction_id))
-
-
-class BeginRO(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "beginRO")
-
-    def execute(self, tick, tm, retry=False):
-        if Transaction(self.para[0], tick, True).transaction_id not in tm.transactions:
-            tm.transactions[Transaction(self.para[0], tick, True).transaction_id] = Transaction(self.para[0], tick, True)
-            for i in range(len(tm.sites)):
-              tm.sites[i].snapshot(tick)
-            return True
-        else:
-            raise KeyError("Dupilcated transaction" + str(Transaction(self.para[0], tick, True).transaction_id))
-
-class Read(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "R")
+    def __str__(self):
+        return str(self.op_t)+','.join(self.para)
 
     def execute(self, tick: int, tm, retry=False):
+        pass
 
-        if retry == False:
-            self.save_to_transaction(tm)
+    def save_to_transaction(self, tm):
+        length = len(self.para)
+        if length == 0:
+            raise TypeError("Cannot perform dump operation after transaction.")
 
-        trans_id, var_id_str = self.para[0], self.para[1]
-        _, var_id = parse_variable_id(var_id_str)
+        if self.para[0] in tm.transactions is False:
+            raise KeyError("Cannot execute ",str(self.op_t), " when transaction doesn't exist.")
 
-        if tm.transactions[trans_id].is_readonly == True:
-            trans_start_tick = tm.transactions[trans_id].tick
+        tm.transactions[self.para[0]].add_operation(self)
+        tm.wait_for_graph.add_operation(self)
 
-            if var_id % 2 == 1:
-                site = tm.get_site(var_id % 10 + 1)
-                if site.up == True:
-                  if var_id in site.snapshots[trans_start_tick]:
-                    if var_id in site.snapshots[trans_start_tick]:
-                      print(tabulate(["Transaction", "Site", var_id_str], [[trans_id, str(site.site_id), str(site.get_snapshot_variable(trans_start_tick, var_id))]]))
-                      return True
-                else:
-                  return False
+    def get_parameters(self):
+        return self.para
 
-            elif var_id % 2 == 1:
-                new_id = var_id % 10 + 1
-                site = tm.get_site(new_id)
-    
-                if site.up == True:
-                  if site.data_manager.check_accessibility(var_id):
-                    if site.lock_manager.try_lock_variable(trans_id, var_id_str, 0):
-                        return do_read(trans_id, var_id, site)
-                    else:
-                        return False
-                else:
-                  return False
-            else:
-                new_id = var_id % 10 + 1
-                site = tm.get_site(new_id)
-                for i in range(len(tm.sites)):
-                    if tm.sites[i].up == False:
-                        continue
-                    elif site.data_manager.check_accessibility(var_id) and site.lock_manager.try_lock_variable(trans_id, var_id_str, 0):
-                        return do_read(trans_id, var_id, site)
-            return False
-        elif var_id % 2 == 1:
-            new_id = var_id % 10 + 1
-            site = tm.get_site(new_id)
- 
-            if site.up == True:
-              if site.data_manager.check_accessibility(var_id):
-                if site.lock_manager.try_lock_variable(trans_id, var_id_str, 0) == False:
-                    return False
-                else:
-                    do_read(trans_id, var_id, site)
-            else:
-              return False
-        else:
-            new_id = var_id % 10 + 1
-            site = tm.get_site(new_id)
-            for i in range(len(tm.sites)):
-                if tm.sites[i].up == False:
-                    continue
-                elif site.data_manager.check_accessibility(var_id) and site.lock_manager.try_lock_variable(trans_id, var_id_str, 0):
-                    return do_read(trans_id, var_id, site)
-        return False
+    def get_op_t(self):
+        return self.op_t
 
 
-class Write(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "W")
-
-    def execute(self, tick: int, tm, retry=False):
-
-        if retry == False:
-            self.save_to_transaction(tm)
-
-        trans_id, var_id_str, write_value = self.para[0], self.para[1], self.para[2]
-        _, var_id = parse_variable_id(var_id_str)
-
-        if var_id % 2 == 1:
-            site = tm.get_site(var_id % 10 + 1)
-            if site.up == True and site.lock_manager.try_lock_variable(trans_id, var_id_str, 1):
-                logs = site.data_manager.log.get(trans_id, {})
-                logs[var_id] = write_value
-                site.data_manager.log[trans_id] = logs
-                return True
-            else:
-              return False
-        else:
-            locked_sites = []
-            for i in range(len(tm.sites)):
-                site = tm.sites[i]
-                if site.up == True:
-                  if site.lock_manager.try_lock_variable(trans_id, var_id_str, 1):
-                    locked_sites.append(site)
-                  else:
-                    for j in range(len(locked_sites)):
-                      locked_sites[j].lock_manager.try_unlock_variable(var_id_str, trans_id)
-                    return False
-                else:
-                  continue
-
-            if not locked_sites:
-                return False
-
-            for i in range(len(locked_sites)):
-                logs = locked_sites[i].data_manager.log.get(trans_id, {})
-                logs[var_id] = int(write_value)
-                locked_sites[i].data_manager.log[trans_id] = logs
-
-            return True
-
-class Dump(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "dump")
-
-    def execute(self, tick: int, tm, retry=False):
-        res = []
-        for i in range(len(tm.sites)):
-          res.append(tm.sites[i].echo())
-        print(tabulate(TABLE_HEADERS, res))
-        return True
-
-class Fail(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "fail")
-
-    def execute(self, tick: int, tm, retry=False):
-        site = tm.get_site(int(self.para[0]))
-        transactions = site.lock_manager.get_involved_transactions()
-        for t in transactions:
-            tm.transactions[t].to_be_aborted = True
-        site.fail()
-        return True
+def parse_variable_id(variable_id):
+    for num, c in enumerate(variable_id, start=1):
+        if c.isdigit() == True:
+            return variable_id[:(num - 1)], int(variable_id[(num - 1):])
 
 
-class Recover(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "recover")
-
-    def execute(self, tick: int, tm, retry=False):
-        tm.get_site(int(self.para[0])).recover()
-        return True
-
-
-class End(Operation):
-    def __init__(self, para):
-        super().__init__(para)
-        Operation.__setattr__(self, "op_t", "end")
-
-    def execute(self, tick: int, tm, retry=False):
-
-        if retry == False:
-            self.save_to_transaction(tm)
-
-        if tm.transactions[self.para[0]].to_be_aborted:
-            tm.abort(self.para[0], 1)
-            return True
-
-        if self.para[0] in tm.blocked_transactions:
-            return False
-
-        print("Transaction ", str(self.para[0]), " commit")
-
-        for site in tm.sites:
-            if site.up:
-              if self.para[0] in site.data_manager.log:
-                for key in site.data_manager.log[self.para[0]].keys():
-                    site.data_manager.set_variable(key, site.data_manager.log[self.para[0]][key])
-                    site.data_manager.is_accessible[key - 1] = True
-                site.data_manager.log.pop(self.para[0])
-              elif tm.transactions[self.para[0]].tick in site.snapshots:
-                site.snapshots.pop(tm.transactions[self.para[0]].tick)
-
-            site.lock_manager.release_transaction_locks(self.para[0])
-
-        if self.para[0] in tm.blocked_transactions:
-            tm.transactions.remove(self.para[0])
-        tm.wait_for_graph.remove_transaction(self.para[0])
-        return True
-
-
-
-class OperationCreator(object):
-    @staticmethod
-    def create(operation, para):
-        operation_type = {"dump": Dump, "W": Write, "R": Read, "beginRO": BeginRO,
-        "begin": Begin, "end": End, "fail": Fail, "recover": Recover}
-        if operation in operation_type:
-          return operation_type[operation](para)
-        else:
-          raise KeyError("Type Error")
-
+def do_read(trans_id, var_id, site):
+    if (trans_id not in site.data_manager.log) or (var_id not in site.data_manager.log[trans_id]):
+        ans = site.data_manager.get_variable(var_id)
+    else:
+        ans = site.data_manager.log[trans_id][var_id]
+    print(tabulate(["Transaction", "Site", "x"+str(var_id)], [[trans_id, str(site.site_id), ans]]))
+    return True
